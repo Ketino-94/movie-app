d0ebfe874fa4ff65434a055f7d52485336e93b18
Ketino-94 <ketino_g@mail.ru>
2019-06-11 11:11:33 +0300

    add project

---
+++ README.md
@@ -0,0 +1,2 @@
+npm install -g <br>
+npm start
---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Filters from "./Filters";
+
+export default class App extends Component {
+  render() {
+    return(
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-4">
+            <div className="card" style={{width: "100%" }}>
+              <div className="card-body">
+                <h3>Фильтры:</h3>
+                <Filters />
+              </div>
+            </div>
+          </div>
+          <div className="col-8">
+            
+          </div>   
+        </div>     
+      </div>
+    );
+  }
+}
