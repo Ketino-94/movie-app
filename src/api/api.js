26b8314eb860f17f9cdf78b66ac957e842b8a48c
Ketino-94 <ketino_g@mail.ru>
2019-06-27 20:54:05 +0300

    add cookie and lofin form

--- src/api/api.js
+++ src/api/api.js
@@ -4,3 +4,24 @@ export const API_KEY_3 = "57d3ab7999dd8eb3bb493818c3ee6668";
 
 export const API_KEY_4 =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjRjYTRmM2E5NzUwZGE1MzQ1MDY0NmNlZDMxMjM5NyIsInN1YiI6IjVhYzlmNWRkOTI1MTQxNjJhZTA1Njk0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fc4f9DVB6pFWh6hIjYe0NCC4pImdmNzDIfi_3Nb3tC4";
+
+export const fetchApi = (url, options = {}) => {
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then(response => {
+        if (response.status < 400) {
+          return response.json();
+        } else {
+          throw response;
+        }
+      })
+      .then(data => {
+        resolve(data);
+      })
+      .catch(response => {
+        response.json().then(error => {
+          reject(error);
+        });
+      });
+  });
+};
