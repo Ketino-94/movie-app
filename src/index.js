ca92c4779f24809dc31551390f5388b47549fdf3
Ketino-94 <ketino_g@mail.ru>
2019-07-19 17:16:06 +0300

    redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./stylesheets/index.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './stylesheets/index.css'
+import { createStore } from 'redux'
 
+const actionCreatorUpdateAuth = payload => {
+	return {
+		type: 'UPDATE_AUTH',
+		payload,
+	}
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const initialState = {
+	user: null,
+	session_id: null,
+	isAuth: false,
+}
 
+const reducerApp = (state = initialState, action) => {
+	switch (action.type) {
+		case 'UPDATE_AUTH':
+			return {
+				...state,
+				user: action.payload.user,
+				session_id: action.payload.session_id,
+				isAuth: true,
+			}
+		default:
+			return state
+	}
+}
+
+const store = createStore(reducerApp)
+store.subscribe(() => {
+	console.log('change', store.getState())
+})
+store.dispatch(
+	actionCreatorUpdateAuth({
+		user: {
+			name: 'Evgeniy',
+		},
+		session_id: 'text',
+	})
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
